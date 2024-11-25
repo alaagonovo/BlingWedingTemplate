3185631d2fbbf46dd778283bf0ca6eafae2d0f4f
alaagonovo <alaa@gonovo.tech>
2024-11-25 14:16:31 +0200

    fix issue

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,22 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   /* config options here */
+//   reactStrictMode: true,
+//   experimental: {
+//     legacyBrowsers: false,
+//     browsersListForSwc: true,
+//   },
+// };
+
+// export default nextConfig;
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactStrictMode: true,
-  experimental: {},
+  experimental: {
+    
+  },
 };
 
 export default nextConfig;
