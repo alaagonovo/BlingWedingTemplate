9917ce4d3138928d19f91b43825acbde99af7b3d
alaagonovo <alaa@gonovo.tech>
2024-11-28 21:35:15 +0200

    coupons Page

--- src/components/coupons/cardcoupon/CardCoupon.tsx
+++ src/components/coupons/cardcoupon/CardCoupon.tsx
@@ -1,27 +1,26 @@
 import Image from "next/image";
 import React, { useEffect, useState } from "react";
 import styles from "./cardcoupone.module.css";
-// import { useIntersectionObserver } from "@/hooks/useIntersectionObserver";
+import { useIntersectionObserver } from "@/hooks/useIntersectionObserver";
 function CardCoupon({ data }: { data: string[] }) {
-  //   const { isVisible, elementRef } = useIntersectionObserver();
+  const { isVisible, elementRef } = useIntersectionObserver();
   const [Index, setIndex] = useState<number>(0);
 
   useEffect(() => {
-    if (data.length === 0) return; // No images to display
+    if (!isVisible || data.length === 0) return; // No images to display
 
     const timer = setInterval(() => {
       if (Index <= 3) {
-        setIndex((prevIndex) => prevIndex + 1);
+        setIndex((prevIndex) => (prevIndex + 1) % data.length);
       } else {
         setIndex(0);
       }
     }, 1000);
-
     // Cleanup timer on unmount
     return () => clearInterval(timer);
-  }, [data, Index]);
+  }, [data, Index, isVisible]);
   return (
-    <div className={styles.card_container} /*ref={elementRef}*/>
+    <div className={styles.card_container} ref={elementRef}>
       {/* {isVisible && ( */}
       <Image
         src={data[Index] || data[0]}
