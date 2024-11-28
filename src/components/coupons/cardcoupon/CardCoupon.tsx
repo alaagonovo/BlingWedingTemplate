bb0722a6f30fb2578ff25212718b8eaf8176961e
alaagonovo <alaa@gonovo.tech>
2024-11-28 21:12:24 +0200

    coupons Page

--- src/components/coupons/cardcoupon/CardCoupon.tsx
+++ src/components/coupons/cardcoupon/CardCoupon.tsx
@@ -1,22 +1,30 @@
 import Image from "next/image";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./cardcoupone.module.css";
 // import { useIntersectionObserver } from "@/hooks/useIntersectionObserver";
 function CardCoupon({ data }: { data: string[] }) {
   //   const { isVisible, elementRef } = useIntersectionObserver();
   const [Index, setIndex] = useState<number>(0);
-  setTimeout(() => {
-    if (Index <= 3) {
-      setIndex((prev) => prev + 1);
-    } else {
-      setIndex(0);
-    }
-  }, 1000);
+
+  useEffect(() => {
+    if (data.length === 0) return; // No images to display
+
+    const timer = setInterval(() => {
+      if (Index <= 3) {
+        setIndex((prevIndex) => prevIndex + 1);
+      } else {
+        setIndex(0);
+      }
+    }, 1000);
+
+    // Cleanup timer on unmount
+    return () => clearInterval(timer);
+  }, [data, Index]);
   return (
     <div className={styles.card_container} /*ref={elementRef}*/>
       {/* {isVisible && ( */}
       <Image
-        src={data[Index] || data[0] || data[1] || data[2] || data[3]}
+        src={data[Index] || data[0]}
         width={100}
         height={100}
         alt="coupon view"
