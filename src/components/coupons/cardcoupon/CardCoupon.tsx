577f288099c33a41669d7070e1c3974e28cc74e3
alaagonovo <alaa@gonovo.tech>
2024-12-03 21:14:49 +0200

    discount percentage

--- src/components/coupons/cardcoupon/CardCoupon.tsx
+++ src/components/coupons/cardcoupon/CardCoupon.tsx
@@ -5,21 +5,22 @@ import { useIntersectionObserver } from "@/hooks/useIntersectionObserver";
 import Link from "next/link";
 
 interface CardCouponProps {
-  data: string[];
+  images: string[];
   altTexts?: string[]; // Optional alt texts for images
   width?: number | string;
   height?: number | string;
   minInterval?: number; // Minimum interval time in milliseconds
-  maxInterval?: number; // Maximum interval time in milliseconds
+  maxInterval?: number;
+  discount: number; // Maximum interval time in milliseconds
 }
 
 const getRandomInterval = (min: number, max: number) =>
   Math.floor(Math.random() * (max - min + 1)) + min;
 
 const CardCoupon: React.FC<CardCouponProps> = ({
-  data,
+  images,
   altTexts = [],
-
+  discount,
   minInterval = 1500,
   maxInterval = 3500,
 }) => {
@@ -27,11 +28,11 @@ const CardCoupon: React.FC<CardCouponProps> = ({
   const [index, setIndex] = useState<number>(0);
 
   const nextImage = useCallback(() => {
-    setIndex((prevIndex) => (prevIndex + 1) % data.length);
-  }, [data.length]);
+    setIndex((prevIndex) => (prevIndex + 1) % images.length);
+  }, [images.length]);
 
   useEffect(() => {
-    if (!isVisible || data.length === 0) return;
+    if (!isVisible || images.length === 0) return;
 
     const cycleImages = () => {
       nextImage();
@@ -45,30 +46,37 @@ const CardCoupon: React.FC<CardCouponProps> = ({
     );
 
     return () => clearTimeout(timerId);
-  }, [isVisible, data.length, minInterval, maxInterval, nextImage]);
+  }, [isVisible, images.length, minInterval, maxInterval, nextImage]);
 
   const altText =
     altTexts[index] ||
-    `Coupon image ${index + 1} of ${data.length}` ||
+    `Coupon image ${index + 1} of ${images.length}` ||
     "Coupon view";
 
   return (
     <div className={styles.card_container} ref={elementRef}>
-      {isVisible && data.length > 0 && (
-        // <div className={styles.image_wrapper}>
-        <Link href="/payment">
-          <Image
-            src={data[index]}
-            width={100}
-            height={100}
-            alt={altText}
-            quality={80}
-            unoptimized={true} // Enable optimization
-            className={styles.coupon_image}
-            loading="lazy"
-          />
-        </Link>
-        // </div>
+      {isVisible && images.length > 0 && (
+        <>
+          <p
+            data-aos="fade-left"
+            className="absolute bg-red-500 bg-opacity-85 py-1 px-2 text-white text-sm font-thin sm:font-medium sm:text-base top-2 sm:top-8 right-0"
+          >
+            {discount}% OFF
+          </p>
+
+          <Link href="/payment">
+            <Image
+              src={images[index]}
+              width={100}
+              height={100}
+              alt={altText}
+              quality={80}
+              unoptimized={true} // Enable optimization
+              className={styles.coupon_image}
+              loading="lazy"
+            />
+          </Link>
+        </>
       )}
     </div>
   );
--- src/components/coupons/couponssection/OtherCoupons.tsx
+++ src/components/coupons/couponssection/OtherCoupons.tsx
@@ -3,13 +3,14 @@ import styles from "./othercoupons.module.css";
 import { Tables } from "../../../../database.types";
 import CardCoupon from "../cardcoupon/CardCoupon";
 function OtherCoupons({ data }: { data: Tables<"vendors">[] }) {
-  {
-    console.log(data);
-  }
   return (
     <section className={styles.coupons_main_grid}>
       {data?.map((item, index) => (
-        <CardCoupon key={index} data={item.images || []} />
+        <CardCoupon
+          key={index}
+          images={item.images || []}
+          discount={item.discount}
+        />
       ))}
     </section>
   );
