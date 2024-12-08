5e3b4fe8cbf18541526d4437a88f3842d359be5e
alaagonovo <alaa@gonovo.tech>
2024-12-08 14:41:35 +0200

    solve slow loading of video

--- src/components/ui/blog/data.ts
+++ src/components/ui/blog/data.ts
@@ -1,5 +1,5 @@
 export const blogData = [
   { vid_src: "/1.mp4", fallback: "/placevid1.webp" },
   { vid_src: "/2.mp4", fallback: "/placevid2.webp" },
-  { vid_src: "/3.mp4", fallback: "/placevid2.webp" },
+  { vid_src: "/3.mp4", fallback: "/placevid3.webp" },
 ];
--- src/components/ui/mobileframe/Mobile.tsx
+++ src/components/ui/mobileframe/Mobile.tsx
@@ -113,7 +113,7 @@ function MobileFrame({ video, image, fallback }: IMobileframe) {
       {
         root: null,
         rootMargin: "300px 0px", // Preload the content slightly before it enters the viewport
-        threshold: 0.1, // Load when 10% of the component is visible
+        threshold: 0.0, // Load when 10% of the component is visible
       }
     );
 
