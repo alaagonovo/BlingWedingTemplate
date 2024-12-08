d281ac221a4c64a954e2e1b7fab779479f375b28
alaagonovo <alaa@gonovo.tech>
2024-12-08 13:50:59 +0200

    edit link videos in mobile frame

--- src/components/ui/blog/data.ts
+++ src/components/ui/blog/data.ts
@@ -1,1 +1,5 @@
-export const blogData = [{vid_src:"1.mp4",fallback_img:'/placevid1'}, {vid_src:"2.mp4",fallback_img:'/placevid2'},{vid_src:"3.mp4",fallback_img:'/placevid3'}];
+export const blogData = [
+  { vid_src: "/1.mp4", fallback_img: "/placevid1" },
+  { vid_src: "/2.mp4", fallback_img: "/placevid2" },
+  { vid_src: "/3.mp4", fallback_img: "/placevid3" },
+];
